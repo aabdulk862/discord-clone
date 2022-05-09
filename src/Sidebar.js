dd7e4d25af1784e50bc90050c839a74675b78f09
aabdulk862 <78934570+aabdulk862@users.noreply.github.com>
2022-05-09 16:21:25 -0400

    text channels

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,22 +1,31 @@
 import React from "react";
 import "./Sidebar.css";
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import AddIcon from '@mui/icons-material/Add';
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import AddIcon from "@mui/icons-material/Add";
+import SidebarChanel from "./SidebarChanel";
 function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar__top">
         <h3>Aabdulk862</h3>
-        <ExpandMoreIcon/>
+        <ExpandMoreIcon />
       </div>
+
       <div className="sidebar__channels">
-         <div className="sidebar__channelsHeader">
-            <div className="sidebar__header">
-                <ExpandMoreIcon/>
-                <h4>Text Channels</h4>
-            </div>
-            <AddIcon className="sidebar__addChannel"/>
-         </div> 
+        <div className="sidebar__channelsHeader">
+          <div className="sidebar__header">
+            <ExpandMoreIcon />
+            <h4>Text Channels</h4>
+          </div>
+          <AddIcon className="sidebar__addChannel" />
+        </div>
+
+        <div className="sidebar__channelsList">
+          <SidebarChanel />
+          <SidebarChanel />
+          <SidebarChanel />
+          <SidebarChanel />
+        </div>
       </div>
     </div>
   );
---
+++ src/SidebarChanel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SidebarChannel.css'
+
+function SidebarChanel({id, channel}) {
+  return (
+    <div className='sidebarChannel'>
+        <h4><span className="sidebarChannel__hash">#</span>Youtube</h4>
+    </div>
+  )
+}
+
+export default SidebarChanel
