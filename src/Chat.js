f44089166dd0f67be0de3e232deadabcaeae911c
aabdulk862 <78934570+aabdulk862@users.noreply.github.com>
2022-05-10 11:04:25 -0400

    message functionality

--- src/Chat.js
+++ src/Chat.js
@@ -10,57 +10,63 @@ import { useSelector } from "react-redux";
 import { selectChannelId, selectChannelName } from "./features/appSlice";
 import { selectUser } from "./features/userSlice";
 import db from "./firebase";
-import firebase from 'firebase/compat/app'; 
+import firebase from "firebase/compat/app";
 function Chat() {
   const user = useSelector(selectUser);
   const channelId = useSelector(selectChannelId);
   const channelName = useSelector(selectChannelName);
   const [input, setInput] = useState("");
-  const [messages, setMessages] = useState([])
+  const [messages, setMessages] = useState([]);
   useEffect(() => {
-    if(channelId){
-      db.collection('channels')
-      .doc(channelId)
-      .collection('messages')
-      .orderBy('timestamp','desc')
-      .onSnapshot((snapshot)=> 
-            setMessages(snapshot.docs.map((doc)=>doc.data()))
+    if (channelId) {
+      db.collection("channels")
+        .doc(channelId)
+        .collection("messages")
+        .orderBy("timestamp", "desc")
+        .onSnapshot((snapshot) =>
+          setMessages(snapshot.docs.map((doc) => doc.data()))
         );
-    }  
+    }
   }, [channelId]);
 
-  const sendMessage = e =>{
+  const sendMessage = (e) => {
     e.preventDefault();
-    db.collection('channels').doc(channelId).collection('messages').
-    add({
+    db.collection("channels").doc(channelId).collection("messages").add({
       message: input,
       user: user,
       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-    })
-  }
+    });
+
+    setInput("");
+  };
 
   return (
     <div className="chat">
       <ChatHeader channelName={channelName} />
 
       <div className="chat__messages">
-        {messages.map((message)=>(
-          <Message/>
-        ))};
+        {messages.map((message) => (
+          <Message
+            message={message.message}
+            user={message.user}
+            timestamp={message.timestamp}
+          />
+        ))}
       </div>
       <div className="chat__input">
         <AddCircleIcon fontSize="large" />
         <form>
           <input
             value={input}
-            disabled ={!channelId}
+            disabled={!channelId}
             onChange={(e) => setInput(e.target.value)}
             placeholder={`Message ${channelName}`}
           />
-          <button disabled ={!channelId} 
-          className="chat__inputButton" 
-          type="submit"
-          onClick={sendMessage}
+          <button
+            disabled={!channelId}
+            className="chat__inputButton"
+            type="submit"
+            onClick={sendMessage}
           >
             Send Message
           </button>
