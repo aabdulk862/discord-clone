584d0a42e6018c289b205ccf536414ba52972643
aabdulk862 <78934570+aabdulk862@users.noreply.github.com>
2022-05-10 21:11:15 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,19 @@
-import firebase from 'firebase/compat/app'; 
-import 'firebase/compat/auth'; 
-import 'firebase/compat/firestore';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 const firebaseConfig = {
-    apiKey: "AIzaSyCJY7-qhfypa70Wx-gjZ7hd0hja84fxidA",
-    authDomain: "discord-clone-db.firebaseapp.com",
-    projectId: "discord-clone-db",
-    storageBucket: "discord-clone-db.appspot.com",
-    messagingSenderId: "119463782030",
-    appId: "1:119463782030:web:6a88a035375b1e35eb8bdf"
-  };
+  apiKey: "AIzaSyCJY7-qhfypa70Wx-gjZ7hd0hja84fxidA",
+  authDomain: "discord-clone-db.firebaseapp.com",
+  projectId: "discord-clone-db",
+  storageBucket: "discord-clone-db.appspot.com",
+  messagingSenderId: "119463782030",
+  appId: "1:119463782030:web:6a88a035375b1e35eb8bdf",
+};
 
-  const firebaseApp = firebase.initializeApp(firebaseConfig);
-  const db = firebaseApp.firestore();
-  const auth = firebase.auth();
-  const provider = new firebase.auth.GoogleAuthProvider();
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
 
-  export {auth,provider};
-  export default db;
+export { auth, provider };
+export default db;
